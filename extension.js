8f76617c0e8c5c6498c264a1e1509b89a2c40b35
Eric Ostrowski <eric.ostrowski@live.com>
2024-09-28 17:58:07 -0500

    Ensure the menu buttons are always added when the extension is enabled and always removed when the extension is disabled

--- extension.js
+++ extension.js
@@ -21,31 +21,46 @@ import {Extension, InjectionManager} from 'resource:///org/gnome/shell/extension
 import {AppMenu} from 'resource:///org/gnome/shell/ui/appMenu.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
-export default class PlainExampleExtension extends Extension {
+export default class EditDesktopFilesExtension extends Extension {
+
+    constructor(metadata) {
+        super(metadata)
+        this.affectedMenus = []
+        this.addedMenuItems = []
+    }
 
     enable() {
         this._injectionManager = new InjectionManager();
 
-        // Extend the AppMenu's setApp method to add an edit button
+        // Extend the AppMenu's open method to add an 'Edit' MenuItem
         // See: https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/js/ui/appMenu.js
-        this._injectionManager.overrideMethod(AppMenu.prototype, 'setApp',
+        this._injectionManager.overrideMethod(AppMenu.prototype, 'open',
             originalMethod => {
                 const metadata = this.metadata;
+                const affectedMenus = this.affectedMenus;
+                const addedMenuItems = this.addedMenuItems;
 
-                return function (app) {
-                    originalMethod.call(this, app);
+                return function (animate) {
 
-                    const appInfo = this._app?.app_info;
-                    if (!appInfo) {
+                    // Suitably awful name to ensure it doesn't conflict with an existing/future property
+                    if (this._editDesktopFilesExtensionMenuItem && this._editDesktopFilesExtensionMenuItem.destroy) {
+                        originalMethod.call(this, animate);
                         return
                     }
 
+                    const appInfo = this._app?.app_info;
+                    if (!appInfo) {
+                        originalMethod.call(this, animate);
+                        return
+                    }
+
+                    // Add the 'Edit' MenuItem
                     let editMenuItem = this.addAction(_('Edit'), () => {
                         GLib.spawn_command_line_async(`gapplication launch org.gnome.TextEditor ${appInfo.filename}`);
                         Main.overview.hide();
                     })
 
-                    // Move the menu item to be after App Details
+                    // Move the 'Edit' MenuItem to be after 'App Details' MenuItem
                     let menuItems = this._getMenuItems()
                     for (let i = 0; i < menuItems.length; i++) {
                         let menuItem = menuItems[i]
@@ -56,6 +71,13 @@ export default class PlainExampleExtension extends Extension {
                             }
                         }
                     }
+
+                    // Keep track of menus that have been affected
+                    this._editDesktopFilesExtensionMenuItem = editMenuItem
+                    affectedMenus.push(this)
+                    addedMenuItems.push(editMenuItem)
+
+                    originalMethod.call(this, animate);
                 };
             }
         );
@@ -64,5 +86,17 @@ export default class PlainExampleExtension extends Extension {
     disable() {
         this._injectionManager.clear();
         this._injectionManager = null;
+
+        // Delete the added properties
+        for (let menu of this.affectedMenus) {
+            delete menu._editDesktopFilesExtensionMenuItem
+        }
+        
+        // Delete all added MenuItems
+        for (let menuItem of this.addedMenuItems) {
+            menuItem.destroy();
+        }
+
+        this.addedMenuItems = []
     }
 }
