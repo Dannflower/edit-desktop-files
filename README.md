3b49c41db861adb3adb2fc29bc50fff0d3f0d7c3
Eric Ostrowski <eric.ostrowski@live.com>
2024-09-28 12:29:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Edit Desktop Files Gnome-Shell Extension
+
+### Commands
+
+Start the nested Wayland session with debug logs enabled + correct display:
+```sh
+G_MESSAGES_DEBUG="GNOME Shell" WAYLAND_DISPLAY=wayland-1 dbus-run-session -- gnome-shell --nested --wayland
+```
+
+Enable the extension in the nested session:
+```sh
+gnome-extensions enable editdesktopfiles@dannflower.github.io
+```
---
+++ extension.js
@@ -0,0 +1,54 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+import {Extension, InjectionManager} from 'resource:///org/gnome/shell/extensions/extension.js';
+import {AppMenu} from 'resource:///org/gnome/shell/ui/appMenu.js';
+
+
+/**
+ * Create a new indicator.
+ *
+ * @param {string} [name] - A name for the indicator
+ * @returns {PanelMenu.Button} A new indicator
+ */
+export function createIndicator(name = 'Unknown') {
+    return new PanelMenu.Button(0.0, name, true);
+}
+
+export default class PlainExampleExtension extends Extension {
+
+    enable() {
+        this._injectionManager = new InjectionManager();
+
+        // Extend the AppMenu's setApp method to add an edit button
+        this._injectionManager.overrideMethod(AppMenu.prototype, 'setApp',
+            originalMethod => {
+                const metadata = this.metadata;
+
+                return function (app) {
+                    console.debug(`${metadata.name}: it's mine now!`);
+                    originalMethod.call(this, app);
+                };
+            }
+        );
+    }
+
+    disable() {
+        this._injectionManager.clear();
+        this._injectionManager = null;
+    }
+}
