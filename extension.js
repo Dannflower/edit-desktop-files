8e19681be1d44497709faf664f4bd10c5d5cb4fa
Eric Ostrowski <eric.ostrowski@live.com>
2026-09-01 10:33:59 -0500

    Use connectObject and disconnectObject for settings signals

--- extension.js
+++ extension.js
@@ -32,7 +32,6 @@ export default class EditDesktopFilesExtension extends Extension {
 
     enable() {
         this._settings = this.getSettings()
-        this._settingsSignalIds = []
         this._injectionManager = new InjectionManager()
         this._modifiedMenus = []
         this._addedEditMenuItems = []
@@ -45,16 +44,19 @@ export default class EditDesktopFilesExtension extends Extension {
         let localizedOpenLocationStr = gettext('Open Entry Location')
 
         // Listen for changes to the 'hide' settings
-        this._settingsSignalIds.push(this._settings.connect('changed::hide-edit-menu-item', (settings, key) => {
-            if (settings.get_boolean(key)) {
-                this.removeEditMenuItems()
-            }
-        }));
-        this._settingsSignalIds.push(this._settings.connect('changed::hide-open-entry-location-menu-item', (settings, key) => {
-            if (settings.get_boolean(key)) {
-                this.removeOpenLocationMenuItems()
-            }
-        }));
+        this._settings.connectObject(
+            'changed::hide-edit-menu-item', (settings, key) => {
+                if (settings.get_boolean(key)) {
+                    this.removeEditMenuItems()
+                }
+            },
+            'changed::hide-open-entry-location-menu-item', (settings, key) => {
+                if (settings.get_boolean(key)) {
+                    this.removeOpenLocationMenuItems()
+                }
+            },
+            this
+        )
 
         // Extend the AppMenu's 'open' method to add an 'Edit' MenuItem
         // See: https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/main/js/ui/appMenu.js
@@ -220,19 +222,14 @@ export default class EditDesktopFilesExtension extends Extension {
         this._addedEditMenuItems = null
         this._addedOpenLocationMenuItems = null
         this._modifiedMenus = null
-        this._settingsSignalIds = null
     }
 
     disconnectSettingsSignals() {
-        if (!this._settings || !this._settingsSignalIds) {
+        if (!this._settings) {
             return
         }
 
-        for (let signalId of this._settingsSignalIds) {
-            this._settings.disconnect(signalId)
-        }
-
-        this._settingsSignalIds = []
+        this._settings.disconnectObject(this)
     }
 
     /**
